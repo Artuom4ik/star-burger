7a51b320a9b928da70a0d48a61f72bdc32be0fb5
Artuom4ik <masterskayaigr123@gmail.com>
2023-12-12 19:16:28 +0300

    Create app

---
+++ place/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Place(models.Model):
+    address = models.CharField(max_length=200, verbose_name="Адрес места", unique=True)
+    latitude = models.FloatField(verbose_name="Широта", null=True)
+    longitude = models.FloatField(verbose_name="Долгота", null=True)
+    update_at = models.DateTimeField('Время запроса к геокодеру', default=timezone.now)
+
+    
