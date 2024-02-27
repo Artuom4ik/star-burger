220af915890c21a820b0e4d9da5ca963fd9d6a04
Artuom4ik <masterskayaigr123@gmail.com>
2024-02-27 19:45:23 +0300

    Add rollbar

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -42,7 +42,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ROLLBAR = {
+    'access_token': env('ROLLBAR_ACCESS_TOKEN'),
+    'environment': 'development' if DEBUG else 'production',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'star_burger.urls'
 
